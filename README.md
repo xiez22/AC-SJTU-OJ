97920dcf8cde049ba2877193914e97be574242aa
谢哲 <xiezhe20001128@163.com>
2019-08-11 23:10:01 +0800

    Initial Submit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AC-SJTU-OJ
 上海交通大学OJ代码自动提交。
+
+## 使用方法
+- 安装Chrome浏览器，ChromeDriver, Python3.7 以及Selenium插件。
+- 运行代码，输入您的用户名和密码，以及您需要AC的题目。
+- 程序会自动拉取代码，并登录您的SJTU-OJ账号提交。
+
+## 注意事项
+- 本代码运行需要安装Chrome浏览器以及ChromeDriver，Python和Selenium插件。
+- 本程序的代码来自ligongzzz的仓库SJTU-OJ。
+- 本程序不会收集您的任何个人信息。
+- 本程序仅供娱乐。
---
+++ test.py
@@ -0,0 +1,59 @@
+# 上海交通大学OJ 自动AC程序
+# 代码来自 ligongzzz SJTU-OJ仓库
+# 仅供娱乐。
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time
+
+username = input('您的用户名：')
+password = input('您的密码：')
+problem_to_solve = input('您要解决哪道题：')
+
+option = webdriver.ChromeOptions()
+browser = webdriver.Chrome(options=option)
+
+try:
+    print("正在获取代码......")
+    browser.get(
+        'https://raw.githubusercontent.com/ligongzzz/SJTU-OJ/master/Code/Project'
+        + problem_to_solve + '/Project' + problem_to_solve + '/源.cpp')
+    print('代码获取成功！')
+    code_to_input = browser.find_element_by_xpath('/html/body/pre').text
+    print(code_to_input)
+
+    browser.get("https://acm.sjtu.edu.cn/OnlineJudge")
+    input_username = browser.find_element(By.NAME, 'username')
+    input_password = browser.find_element(By.NAME, 'password')
+    btn_login = browser.find_element(By.NAME, 'action')
+
+    actions = ActionChains(browser)
+    actions.send_keys_to_element(input_username, username)
+    actions.send_keys_to_element(input_password, password)
+    actions.click(btn_login)
+    actions.perform()
+
+    time.sleep(1)
+    browser.get("https://acm.sjtu.edu.cn/OnlineJudge/submit")
+
+    input_problem = browser.find_element(By.NAME, 'problem')
+    input_code = browser.find_element(By.NAME, 'code')
+    btn_submit = browser.find_element(
+        By.XPATH, '//*[@id="wrap"]/div/form/fieldset/div[4]/button')
+
+    actions = ActionChains(browser)
+    actions.send_keys_to_element(input_problem, problem_to_solve)
+    actions.send_keys_to_element(input_code, code_to_input)
+    actions.click(btn_submit)
+    actions.perform()
+
+    browser.refresh()
+
+    for _ in range(10):
+        time.sleep(1)
+        browser.refresh()
+except:
+    print('抱歉！出现错误')
+finally:
+    browser.close()
